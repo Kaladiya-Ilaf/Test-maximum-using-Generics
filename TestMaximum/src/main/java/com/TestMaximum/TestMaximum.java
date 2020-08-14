c5f32bdf9d8df115801f6dd2404bf920b718295b
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-14 09:43:39 +0530

    Refactored : class as generic to take and compare  type of variable and find maximum.

--- TestMaximum/src/main/java/com/TestMaximum/TestMaximum.java
+++ TestMaximum/src/main/java/com/TestMaximum/TestMaximum.java
@@ -1,6 +1,22 @@
 package com.TestMaximum;
 
-public class TestMaximum {
+public class TestMaximum<E extends Comparable<E>> {
+    E value1;
+    E value2;
+    E value3;
+
+    TestMaximum(){
+
+    }
+    TestMaximum(E value1, E value2, E value3){
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public E findMaximum(){
+        return findMaximumValue(value1, value2, value3);
+    }
 
     public <E extends Comparable> E findMaximumValue(E value1, E value2, E value3){
         if (value1.compareTo(value2) > 0 && value1.compareTo(value3) > 0){
