bc2364f47d217b22e14e9681c632b3c07a4197d7
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-14 07:33:07 +0530

    Modified: Function name to be specific for integers.

--- TestMaximum/src/test/java/com/TestMaximum/TestMaximumTests.java
+++ TestMaximum/src/test/java/com/TestMaximum/TestMaximumTests.java
@@ -5,20 +5,20 @@ import org.junit.Test;
 
 public class TestMaximumTests {
     @Test
-    public void givenNumbers_whenMaximumAtPosition1_shouldReturnPosition1 () {
+    public void givenIntegers_whenMaximumAtPosition1_shouldReturnPosition1 () {
         TesMaximum tesMaximum = new TesMaximum(9, 4, 3);
         int maximum = tesMaximum.findMaximumNumber();
         Assert.assertEquals(9, maximum);
     }
     @Test
-    public void givenNumbers_whenMaximumAtPosition2_shouldReturnPosition2 () {
+    public void givenIntegers_whenMaximumAtPosition2_shouldReturnPosition2 () {
         TesMaximum tesMaximum = new TesMaximum(5, 7, 2);
         int maximum = tesMaximum.findMaximumNumber();
         Assert.assertEquals(7, maximum);
     }
 
     @Test
-    public void givenNumbers_whenMaximumAtPosition3_shouldReturnPosition3 () {
+    public void givenIntegers_whenMaximumAtPosition3_shouldReturnPosition3 () {
         TesMaximum tesMaximum = new TesMaximum(10, 5, 17);
         int maximum = tesMaximum.findMaximumNumber();
         Assert.assertEquals(17, maximum);
